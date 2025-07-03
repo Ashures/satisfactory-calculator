c82f8d40b07d74bfd200771d7036b189f3134120
Ashures <ashuresyt@gmail.com>
2025-07-04 03:26:54 +1200

    build: Connected services to emulators in development mode

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB7BxRl9LumkXjcVct25ttvCff7cWFY_kY",
@@ -9,4 +11,11 @@ const firebaseConfig = {
   appId: "1:162645621086:web:5523ff8a7cb4c60e92b7cf"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+  connectFirestoreEmulator(db, "127.0.0.1", 8080);
+}
