90b822f91e7a204895b938c7e2879a48de385306
Ashures <ashuresyt@gmail.com>
2025-07-04 09:02:46 +1200

    feat: Added Item component to display Item info

---
+++ src/components/items/Item.tsx
@@ -0,0 +1,13 @@
+import type { ItemInfo } from "../../libs/types";
+
+const Item: React.FC<ItemInfo> = (props) => {
+  const { id, displayName, iconUrl } = props;
+  return (
+    <div className="item" id={id}>
+      <h2>{displayName}</h2>
+      <img src={`images/items/${iconUrl}`} alt="" />
+    </div>
+  );
+};
+
+export default Item;
