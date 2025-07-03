eb74c08fd7924e86a57e8c05db7fbcce4088392d
Ashures <ashuresyt@gmail.com>
2025-07-04 03:32:48 +1200

    feat: Added logo to Navbar with styling

--- src/components/main/Navbar.tsx
+++ src/components/main/Navbar.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router";
+
 const Navbar: React.FC = () => {
   return (
     <nav>
-      
+      <Link to="/">
+        <div className="logo"></div>
+      </Link>
     </nav>
   );
 };
