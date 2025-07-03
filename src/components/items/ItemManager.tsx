4773dde311810fb6dc65a7f5700ba635667a0ca5
Ashures <ashuresyt@gmail.com>
2025-07-04 09:05:30 +1200

    feat: Created ItemManager to display all items and future selected items

---
+++ src/components/items/ItemManager.tsx
@@ -0,0 +1,11 @@
+import ItemList from "./ItemList";
+
+const ItemManager: React.FC = () => {
+  return (
+    <div className="item-manager">
+      <ItemList />
+    </div>
+  );
+};
+
+export default ItemManager;
